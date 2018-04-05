8c4160e0f4616f67bd44762aa8913d4752b5cab2
Richard Gordon <richgordon95@gmail.com>
2018-04-04 22:56:10 -0400

    add first fave place

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
+
+src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBs2Q2XLa17xwLXa_nzdWG8AN2nrJ6dBno&callback=initMap"
+
 function initMap() {
-	var uluru = {lat: -25.363, lng: 131.044};
+	var uluru = {lat: 40.7128, lng: -74.0060};
 	var map = new google.maps.Map(document.getElementById('map'), {
-		zoom: 4,
+		zoom: 18,
 		center: uluru
 	});
 	var marker = new google.maps.Marker({
